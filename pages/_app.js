50d7b8c43f629106ef72232098634d80a9782a21
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-08-12 21:44:27 -0500

    Implement fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '@styles/resetalize.min.css';
 import '@styles/globals.css';
+import '@styles/neue-hans-kendrick.css';
 
 const MyApp = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
